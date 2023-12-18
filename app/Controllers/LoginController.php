aafd52cf144fd2af99b820837b4ead0649dbd367
zeyad ahmed <zeyadmokled@hotmail.com>
2023-12-19 01:29:02 +0200

    make the template for registration with db implementation

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -19,5 +19,6 @@ class LoginController
 
     public function store()
     {
+
     }
 }
--- routes.php
+++ routes.php
@@ -2,6 +2,7 @@
 
 use App\Controllers\HomeController;
 use App\Controllers\LoginController;
+use App\Controllers\RegistrateController;
 use App\Route;
 
 
@@ -9,5 +10,7 @@ Route::get("/", [HomeController::class, "index"]);
 
 Route::get("/login", [LoginController::class, "show"]);
 Route::post("/login", [LoginController::class, "store"]);
+Route::get("/registrate",[RegistrateController::class,"show"]);
+Route::post("/registrate",[RegistrateController::class,"store"]);
 
 //Route::get("/users/(\\d+)", [HomeController::class, "show"]);
