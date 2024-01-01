3d321c96bac9a7b6f745e81e0a2fa4b697e30b7c
Peter Anton <peto4eskander69@gmail.com>
2024-01-01 21:25:08 +0200

    make table and report but not complete

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,11 @@
+@props(['car'])
+
+    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+        <div class="flex items-center">
+            <div class="text-sm font-medium text-gray-900">
+                {{ $car}}
+            </div>
+        </div>
+</td>
+
+
